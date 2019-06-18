be5adcfe93cad4fedf785ce136ad0a3c2d51a8c7
Bláckfu <b.van.rheenen@bmcr.nl>
2019-06-18 18:37:18 +0200

    Fixed NumberFormatException if you close the filter-by-rating popup without input.

--- src/components/dialogs/reports/MasterclassFilterPlayerDialog.java
+++ src/components/dialogs/reports/MasterclassFilterPlayerDialog.java
@@ -15,43 +15,51 @@ public class MasterclassFilterPlayerDialog extends InputDialog {
     MasterclassProvider provider = new MasterclassProvider();
 
     public MasterclassFilterPlayerDialog() {
-        int rating = Integer.parseInt(JOptionPane.showInputDialog(null, "Op welke minimale rating wilt u filtreren?"));
+        int rating = 0;
 
         try {
-            ResultSet rs = provider.filterPlayersByRating(rating);
+            rating = Integer.parseInt(JOptionPane.showInputDialog(null, "Op welke minimale rating wilt u filtreren?"));
+        } catch (NumberFormatException nfe) {
+            // Do nothing. Just close the popup as default.
+        }
 
-            ResultSetMetaData rsmd = rs.getMetaData();
+        if(rating != 0) {
+            try {
+                ResultSet rs = provider.filterPlayersByRating(rating);
 
-            JTable tbl = new JTable();
-            DefaultTableModel dtm = new DefaultTableModel(0, 0);
+                ResultSetMetaData rsmd = rs.getMetaData();
 
-            String header[] = new String[rsmd.getColumnCount()];
+                JTable tbl = new JTable();
+                DefaultTableModel dtm = new DefaultTableModel(0, 0);
 
-            for(int i = 0; i < rsmd.getColumnCount(); i++) {
-                header[i] = rsmd.getColumnName(i + 1);
+                String header[] = new String[rsmd.getColumnCount()];
+
+                for (int i = 0; i < rsmd.getColumnCount(); i++) {
+                    header[i] = rsmd.getColumnName(i + 1);
+                }
+
+                dtm.setColumnIdentifiers(header);
+
+                tbl.setModel(dtm);
+
+                while (rs.next()) {
+                    dtm.addRow(new Object[]{rs.getObject(1).toString(), rs.getObject(2).toString(), rs.getObject(3).toString()});
+                }
+
+                JPanel panel = new JPanel();
+                panel.setLayout(new GridLayout());
+                JScrollPane sp = new JScrollPane(tbl);
+                panel.add(sp);
+
+                JDialog tableDialog = new JDialog();
+                tableDialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                tableDialog.setContentPane(panel);
+
+                tableDialog.pack();
+                tableDialog.setVisible(true);
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-
-            dtm.setColumnIdentifiers(header);
-
-            tbl.setModel(dtm);
-
-            while (rs.next()) {
-                dtm.addRow(new Object[] { rs.getObject(1).toString(), rs.getObject(2).toString(), rs.getObject(3).toString() });
-            }
-
-            JPanel panel = new JPanel();
-            panel.setLayout(new GridLayout());
-            JScrollPane sp = new JScrollPane(tbl);
-            panel.add(sp);
-
-            JDialog tableDialog = new JDialog();
-            tableDialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-            tableDialog.setContentPane(panel);
-
-            tableDialog.pack();
-            tableDialog.setVisible(true);
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 }
