b9cefbf0560ea0ddbcdf20a473747a07d5a8d166
Lucas Spits <spits.lucas@gmail.com>
2019-06-18 13:52:24 +0200

    Add ability to attach SelectionListeners. Add docs

--- src/components/TablePanel.java
+++ src/components/TablePanel.java
@@ -1,9 +1,13 @@
 package components;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.TableModel;
 import java.awt.*;
 
+/**
+ * Container for a {@link JTable}
+ */
 public class TablePanel extends JPanel {
     private JTable table;
 
@@ -20,23 +24,54 @@ public class TablePanel extends JPanel {
         this.add(tableWithScroll, BorderLayout.CENTER);
     }
 
+    /**
+     * Invokes {@link JTable#getColumnCount()}
+     * @return the result of {@link JTable#getColumnCount()}
+     */
     public int getColumnCount() {
         return table.getColumnCount();
     }
 
+    /**
+     * Invokes {@link JTable#getSelectedRow()}
+     * @return the result of {@link JTable#getSelectedRow()}
+     */
     public int getSelectedRow() {
         return table.getSelectedRow();
     }
 
+    /**
+     * Invokes {@link JTable#getSelectedRows()}
+     * @return the result of {@link JTable#getSelectedRows()}
+     */
     public int[] getSelectedRows() {
         return table.getSelectedRows();
     }
 
+    /**
+     * Invokes {@link JTable#getSelectedColumn()}
+     * @return the result of {@link JTable#getSelectedColumn()}
+     */
     public int getSelectedColumn() {
         return table.getSelectedColumn();
     }
 
+    /**
+     * Get the model of the current table
+     * @return The {@link TableModel} of the table
+     */
     public TableModel getModel() {
         return table.getModel();
+    }
+
+    /**
+     * Adds a selection listener to the {@link JTable}
+     * @param listener the {@link ListSelectionListener} to add
+     */
+    public void addSelectionListener(ListSelectionListener listener) {
+        ListSelectionModel selectionModel = table.getSelectionModel();
+        if(selectionModel != null) {
+            selectionModel.addListSelectionListener(listener);
+        }
     }
 }
