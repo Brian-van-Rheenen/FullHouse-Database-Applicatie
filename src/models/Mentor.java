9a7110f7f92544c361257bcf15ed6fb76306678a
Lucas Spits <spits.lucas@gmail.com>
2019-06-21 12:09:41 +0200

    Update model with table fields

--- src/models/Mentor.java
+++ src/models/Mentor.java
@@ -1,4 +1,55 @@
 package models;
 
+/**
+ * Represents a {@link Mentor} for {@link Masterclass}
+ */
 public class Mentor {
+
+    private int id;
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    public Mentor(int id, String name, String phoneNumber, String email) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public Mentor(String name, String phoneNumber, String email) {
+        this(-1, name, phoneNumber, email);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
