ad311cccf8e9eb42b180f0a3d23f23361616ca78
SBDutchie <sbdutchey@gmail.com>
2019-06-24 20:22:02 +0200

    Changed version number

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
  * Een database applicatie voor het fictieve bedrijf FullHouse.
  *
  * @author  Brian van Rheenen, Sam Bitter, Lucas Spits en David Hu.
- * @version 1.0
- * @since   07-06-2019
+ * @version 1.1
+ * @since   24-06-2019
  */
 
 public class Main {
